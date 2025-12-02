cf5132d11a62ef2a9ff22be54001b2f9ebd64e26
Jesse Falconer <jesse.falconer@spark.re>
2025-12-02 07:55:22 +0000

    incomplete answer part b

--- day1/1b.rb
+++ day1/1b.rb
@@ -1,13 +1,14 @@
-# SIGNEDNESS = { "L" => -1, "R" => 1 }.freeze
-# current_position = 50
-# counter = 0
+SIGNEDNESS = { "L" => -1, "R" => 1 }.freeze
+current_position = 50
+counter = 0
 
-# File.foreach("sample.txt").each do |command|
-#   vector = SIGNEDNESS[command[0]] * (command[1..].to_i % 100)
-#   current_position = (current_position + vector) % 100
-#   counter += 1 if current_position.zero?
-# end
+File.foreach("sample.txt").each do |command|
+  vector = SIGNEDNESS[command[0]] * (command[1..].to_i % 100)
+  scalar = SIGNEDNESS[command[0]] * command[1..].to_i
+  counter += ((current_position + scalar) / 100).abs
+  counter -= 1 if current_position.zero? && scalar.negative?
+  current_position = (current_position + vector) % 100
+  counter += 1 if current_position.zero?
+end
 
-# puts counter
-
-# Adapt me
+puts counter # off by 7?
