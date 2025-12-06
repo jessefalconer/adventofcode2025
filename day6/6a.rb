3c2df40fffa8d15fe1809757cae9ddf16e37cd36
Jesse Falconer <jesse.falconer@spark.re>
2025-12-06 22:59:52 +0000

    fix naming, excessive checks

--- day6/6a.rb
+++ day6/6a.rb
@@ -1,8 +1,8 @@
 total = File.readlines("day6/sample.txt", chomp: true)
   .map { _1.strip.split(/\s+/) }
   .transpose
-  .sum do |*numbers, operand|
-    numbers.map(&:to_i).reduce(operand.to_sym)
+  .sum do |*numbers, operator|
+    numbers.map(&:to_i).reduce(operator.to_sym)
   end
 
 puts total
--- day6/6b.rb
+++ day6/6b.rb
@@ -9,19 +9,18 @@ def blank_column?(index)
 end
 
 total = 0
-minor_operand = nil
-minor_numbers = []
+minor_operators, minor_numbers = nil, []
 
 OPERANDS.each_with_index do |operand, index|
   next if blank_column?(index)
   
-  minor_operand ||= operand.to_sym
+  minor_operators ||= operand.to_sym
   minor_numbers << MATRIX.column(index).to_a.join.to_i
   
-  next unless blank_column?(index + 1) || index + 1 == OPERANDS.size
+  next unless blank_column?(index + 1)
   
-  total += minor_numbers.reduce(minor_operand)
-  minor_operand, minor_numbers = nil, []
+  total += minor_numbers.reduce(minor_operators)
+  minor_operators, minor_numbers = nil, []
 end
 
 puts total
