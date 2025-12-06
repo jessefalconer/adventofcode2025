66f072ec3c174d50e6493ccb94ecaa6aca956a20
Jesse Falconer <jesse.falconer@spark.re>
2025-12-06 23:02:57 +0000

    fix naming

--- day6/6b.rb
+++ day6/6b.rb
@@ -1,7 +1,7 @@
 require "matrix"
   
 DATA = File.readlines("day6/sample.txt", chomp: true).map { _1.chars }.freeze
-OPERANDS = DATA.last.freeze
+OPERATORS = DATA.last.freeze
 MATRIX = Matrix[*DATA[0..-2]].freeze
 
 def blank_column?(index)
@@ -9,18 +9,18 @@ def blank_column?(index)
 end
 
 total = 0
-minor_operators, minor_numbers = nil, []
+minor_operator, minor_numbers = nil, []
 
-OPERANDS.each_with_index do |operand, index|
+OPERATORS.each_with_index do |operator, index|
   next if blank_column?(index)
   
-  minor_operators ||= operand.to_sym
+  minor_operator ||= operator.to_sym
   minor_numbers << MATRIX.column(index).to_a.join.to_i
   
   next unless blank_column?(index + 1)
   
-  total += minor_numbers.reduce(minor_operators)
-  minor_operators, minor_numbers = nil, []
+  total += minor_numbers.reduce(minor_operator)
+  minor_operator, minor_numbers = nil, []
 end
 
 puts total
