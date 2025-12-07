c29912cbeecac5cd2fa8625b8cccd43fb33a8b69
Jesse Falconer <jesse.falconer@spark.re>
2025-12-07 22:54:54 +0000

    Remove whitespace

--- day7/7b.rb
+++ day7/7b.rb
@@ -1,28 +1,31 @@
-BEAM     = "S".freeze
-SPLITTER = "^".freeze
-OPEN     = ".".freeze
-GRID = File.readlines("day7/sample.txt", chomp: true).map(&:chars).freeze
-cache = Hash.new(0)
+# BEAM = "S".freeze
+# SPLITTER = "^".freeze
+# OPEN = ".".freeze
+# GRID = File.readlines("day7/sample.txt", chomp: true).map(&:chars).freeze
+# cache = Hash.new(0)
 
-GRID.each_with_index do |row, row_index|
-  row.each_with_index do |char, col_index|
-    key = [row_index, col_index]
-    node_visits = cache[key]
+# GRID.each_with_index do |row, row_index|
+#   row.each_with_index do |char, col_index|
+#     key = [row_index, col_index]
+#     node_visits = cache[key]
 
-    case char
-    when OPEN
-      cache[[row_index + 1, col_index]] += node_visits
-    when SPLITTER
-      [-1, 1].each do |offset|
-        adjacent = col_index + offset
-        next if adjacent < 0 || adjacent >= row.size
+#     case char
+#     when OPEN
+#       cache[[row_index + 1, col_index]] += node_visits
+#     when SPLITTER
+#       [-1, 1].each do |offset|
+#         adjacent = col_index + offset
+#         next if adjacent < 0 || adjacent >= row.size
 
-        cache[[row_index + 1, adjacent]] += node_visits
-      end
-    when BEAM
-      cache[[row_index + 1, col_index]] += 1
-    end
-  end
-end
+#         cache[[row_index + 1, adjacent]] += node_visits
+#       end
+#     when BEAM
+#       cache[[row_index + 1, col_index]] += 1
+#     end
+#   end
+# end
 
-puts cache.select { _1[0] == GRID.size }.values.sum
+# puts cache.select { _1[0] == GRID.size }.values.sum
+
+
+puts cache.inspect
